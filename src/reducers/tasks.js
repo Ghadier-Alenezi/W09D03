f00943684f0689385aab9c60e70b923a82e0f087
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2021-12-09 07:54:03 +0300

    update task reducer

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -1,22 +1,54 @@
 const initialState = {
-  allTasks: [],
+  allTasks: null,
 };
-const getAllTasks = (state = initialState, action) => {
+const taskReducer = (state = initialState, action) => {
   const { type, payload } = action;
   switch (type) {
     case "GETTASKS":
       const { allTasks } = payload;
+      return { allTasks };
+    case "CREATE":
+      const { newTask } = payload;
+      return { allTasks };
+    case "UPDATE":
+      const { updeateTask } = payload;
+      return { allTasks };
+    case "DELETE":
+      const { deleteTask } = payload;
       return { allTasks };
 
     default:
       return state;
   }
 };
-export default getAllTasks;
+export default taskReducer;
 
 export const getTasks = (data) => {
+      // console.log("data", data);
+
   return {
     type: "GETTASKS",
+    payload: data.data,
+  };
+};
+
+export const newTask = (data) => {
+  return {
+    type: "CREATE",
     payload: data,
+  };
+};
+
+export const updateTask = (data) => {
+  // return {
+  //   type: "UPDATE",
+  //   payload: data,
+  // };
+};
+
+export const deleteTask = (data) => {
+  return {
+    type: "DELETE",
+    payload: "",
   };
 };
